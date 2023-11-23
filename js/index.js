81ea24cb3a26c74484dbaaad43e10b153f789df6
Lucie Yarish <lucieyarish@hotmail.com>
2023-11-23 06:56:35 +0100

    Prevent default behavior on form submit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const modal = document.getElementById('modal');
 const closeBtn = document.getElementById('modal-close-btn');
+const consentForm = document.getElementById('consent-form');
 
 setTimeout(function () {
   modal.style.display = 'inline';
@@ -7,4 +8,8 @@ setTimeout(function () {
 
 closeBtn.addEventListener('click', function () {
   modal.style.display = 'none';
+});
+
+consentForm.addEventListener('submit', function (e) {
+  e.preventDefault();
 });
