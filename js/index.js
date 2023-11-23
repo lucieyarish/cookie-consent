064095a45f501337913059526c22c4a27494238a
Lucie Yarish <lucieyarish@hotmail.com>
2023-11-23 06:59:14 +0100

    Display info msg after form submission

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const modal = document.getElementById('modal');
 const closeBtn = document.getElementById('modal-close-btn');
 const consentForm = document.getElementById('consent-form');
+const modalText = document.getElementById('modal-text');
 
 setTimeout(function () {
   modal.style.display = 'inline';
@@ -12,4 +13,13 @@ closeBtn.addEventListener('click', function () {
 
 consentForm.addEventListener('submit', function (e) {
   e.preventDefault();
+  modalText.innerHTML = '';
+  modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="assets/images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>
+  `;
 });
